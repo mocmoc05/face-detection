32ce862c8bc8e888ab38d6ae540b53e6a62e1149
Hoàn Vũ Văn <hoanvv.tb@gmail.com>
2020-01-24 20:33:42 +0700

    init proj

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+class App extends PureComponent {
+    render() {
+        return (
+            <View style={styles.container}>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    style={styles.preview}
+                    type={RNCamera.Constants.Type.front}
+                    flashMode={RNCamera.Constants.FlashMode.off}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                        title: 'Permission to use audio recording',
+                        message: 'We need your permission to use your audio',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                        console.log(barcodes);
+                    }}
+                    onFacesDetected={(faces) => {console.log({faces: faces.faces})}}
+                    onFaceDetectionError={(error) => console.log({error})}
+                    faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.accurate}
+                    faceDetectionLandmarks={RNCamera.Constants.FaceDetection.Landmarks.all}
+                    faceDetectionClassifications={RNCamera.Constants.FaceDetection.Classifications.all}
+                />
+                <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+                    <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+                        <Text style={{ fontSize: 14 }}> SNAP </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+
+    takePicture = async() => {
+        if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options);
+            console.log(data.uri);
+        }
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    },
+});
+
+export default App;
